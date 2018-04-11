52f8968b7a2c677e80781e39884e46a33bbc46d8
chang1331 <chang1.3@outlook.com>
2018-04-11 00:06:22 -0500

    Implemeta Heuristica Piezas (falta hPos)

--- alfaBeta.lisp
+++ alfaBeta.lisp
@@ -1,6 +1,33 @@
-(defvar h finJuego)
+(setq h 0 finJuego 0)
 (setq tablero '() pila nil cerrado nil nodo nil poda nil)
-(setq hPieza 0 hPos 0 hmovT 0 hpatron 0 sumadist 0)
+(setq hPieza 0 hPos 0 hmovT 0 patronH 0 sumadist 0)
+
+(setq tabPeonY '((nil 0 nil 0 nil 0 nil 0)
+			     (6 nil 5 nil 5 nil 5 nil)
+				 (nil 4 nil 4 nil 4 nil 5)
+				 (4 nil 3 nil 3 nil 3 nil)
+				 (nil 2 nil 2 nil 2 nil 3)
+				 (2 nil 1 nil 1 nil 1 nil)
+				 (nil 0 nil 0 nil 0 nil 1)
+				 (3 nil 3 nil 3 nil 3 nil)))
+
+(setq tabPeonT '((nil 3 nil 3 nil 3 nil 3)
+			     (1 nil 0 nil 0 nil 0 nil)
+				 (nil 1 nil 1 nil 1 nil 2)
+				 (3 nil 2 nil 2 nil 2 nil)
+				 (nil 3 nil 3 nil 3 nil 4)
+				 (5 nil 4 nil 4 nil 4 nil)
+				 (nil 5 nil 5 nil 5 nil 6)
+				 (0 nil 0 nil 0 nil 0 nil)))
+
+(setq tabRey '((nil 0 nil 0 nil 0 nil 0)
+			   (0 nil 1 nil 1 nil 1 nil)
+			   (nil 1 nil 2 nil 2 nil 0)
+			   (0 nil 2 nil 4 nil 1 nil)
+			   (nil 1 nil 4 nil 2 nil 0)
+			   (0 nil 2 nil 2 nil 1 nil)
+			   (nil 1 nil 1 nil 1 nil 0)
+			   (0 nil 0 nil 0 nil 0 nil)))
 ;nodo=(id padre cMov inix iniy finx finy  v)
 
 
@@ -28,11 +55,43 @@
 (defun generaNodo(nodo depth))
 
 (defun calculaH()
-	(setq hPieza cuentaPiezas()))
+	(setq h (+ (cuentaPiezasH) (posTab) (patrones))))
 
-(defun cuentaPiezas()
-	())
 
+
+(defun cuentaPiezasH()
+(let ((c 0))                      
+  (loop for x in tablero   ;x= primer elemento de lista Tablero (nil val1 nil val2 nil val3 nil val4)
+      do (cond((EQ (nth 0 x) nil) (loop for i from 1 to 7 by 2
+      								do  (cond((EQ (nth i x) 0))				 ;pos vacia
+      										 ((EQ (nth i x) 1) (incf c 10))  ;pos con mi peón
+      										 ((EQ (nth i x) 2) (incf c 13))  ;pos con mi rey
+      										 ((EQ (nth i x) -1) (decf c 10)) ;pos con su peón
+      										 (t (decf c 13)))))				 ;pos con su rey
+      			(t (loop for i from 0 to 6 by 2
+						do (cond((EQ (nth i x) 0))              ;pos vacia     
+									((EQ (nth i x) 1) (incf c 10))  ;pos con mi peón
+									((EQ (nth i x) 2) (incf c 13))  ;pos con mi rey
+									((EQ (nth i x) -1) (decf c 10)) ;pos con su peón
+									(t (decf c 13)))))))            ;pos con su rey
+									c))          
+	
+
+(defun posTab()
+(loop for i from 0 to 8   ;x= primer elemento de lista Tablero (nil val1 nil val2 nil val3 nil val4)
+  do (cond((EQ (mod i 2) 0) (loop for j from 1 to 7 by 2
+  								do  (cond((EQ (nth j (nth i tablero)) 0))				 ;pos vacia
+  										 ((EQ (nth j (nth i tablero)) 1) (incf hPos 10))  ;pos con mi peón
+  										 ((EQ (nth i x) 2) (incf c 13))  ;pos con mi rey
+  										 ((EQ (nth i x) -1) (decf c 10)) ;pos con su peón
+  										 (t (decf c 13))))				 ;pos con su rey
+  			(t (loop for j from 0 to 6 by 2
+					do (cond((EQ (nth j x) 0))              ;pos vacia     
+								((EQ (nth i x) 1) (incf c 10))  ;pos con mi peón
+								((EQ (nth i x) 2) (incf c 13))  ;pos con mi rey
+								((EQ (nth i x) -1) (decf c 10)) ;pos con su peón
+								(t (decf c 13)))))))           ;pos con su rey
+								
 
 (defun revierteTab())
 
